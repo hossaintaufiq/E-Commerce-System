d0f1239f49aa0aa29737ddd890fcd5d11a0195ce
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-10-17 11:02:36 +0600

    testing cmd

---
+++ E_Commerce_System/src/e_commerce_system/E_Commerce_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package e_commerce_system;
+
+/**
+ *
+ * @author Hp
+ */
+public class E_Commerce_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ecommerce  system based on java");
+    }
+    
+}
